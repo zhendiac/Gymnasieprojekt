d2e6a80f82fff9d91f66e3c15ec80f2ac447150f
Fuwzie <zypeh@live.com>
2016-10-25 13:49:34 +0200

    Tog bort oanvänd kod

--- project.php
+++ project.php
@@ -16,31 +16,5 @@
 <div class="box2">Trigonometri</div>
 <div class="box3">Geometri</div>
 
-<script type="text/javascript">
-	 $(document).ready(function () {
-    $('#nav-list-example li div.back').hide().css('left', 0);
-    
-    function mySideChange(front) {
-        if (front) {
-            $(this).parent().find('div.box').show();
-            $(this).parent().find('div.back').hide();
-            
-        } else {
-            $(this).parent().find('div.box').hide();
-            $(this).parent().find('div.back').show();
-        }
-    }
-    
-    $('#nav-list-example li').hover(
-        function () {
-            $(this).find('div').stop().rotate3Di('flip', 250, {direction: 'clockwise', sideChange: mySideChange});
-        },
-        function () {
-            $(this).find('div').stop().rotate3Di('unflip', 500, {sideChange: mySideChange});
-        }
-    );
-});
-</script>
-
 </body>
 </html>
