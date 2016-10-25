e48d3e2f9b6701cec72a132e2dd324fed608eeb0
Fuwzie <zypeh@live.com>
2016-10-25 13:34:57 +0200

    css integration

--- project.php
+++ project.php
@@ -2,119 +2,19 @@
 <html>
 <head>
 	<title>Matteprojekt</title>
-</head>
-<body>
 
+<link rel="stylesheet" type="text/css" href="style.css">
 <script type="text/javascript" src="/js/jquery.js"></script>
 <script type="text/javascript" src="/js/base.js"></script>
 <script type="text/javascript" src="/js/rotate3di.js"></script>
 
-<style type="text/css">
+</head>
 
-body {
-	background-image: url(img/bg.jpg);
-	background-size: cover;
-	font-family: "Ubuntu", sans-serif;
-}
-
-.box1{
-		height: 35px;
-		width: 120px;
-		background-color: #42f4ce;
-		color:black;
-		text-align: center;
-		font-weight: bold;
-		border-radius: 4px;
-		margin-top: 120px;
-		margin-left: 600px;
-		font-size: 25px;
-		padding: 5px;
-}
-.box2{
-		height: 35px;
-		width: 130px;
-		background-color: #42f4ce;
-		color:black;
-		text-align: center;
-		font-weight: bold;
-		border-radius: 4px;
-		float: left;
-		margin-left: 200px;
-		font-size: 25px;
-		padding:5px;
-}
-.box3{
-		height: 35px;
-		width: 130px;
-		background-color: #42f4ce;
-		color:black;
-		text-align: center;
-		font-weight: bold;
-		border-radius: 4px;
-		float: right;
-		margin-right: 200px;
-		font-size: 25px;
-		padding:5px;
-}
-
-#box {
-	width:250px;
-	height:250px;
-	border:3px solid #000;
-	background-color: yellow;
-	display: none;
-	
-}
-#back {
-	width:250px;
-	height:250px;
-	border:3px solid #000;
-	background-color: red;
-	display: none;
-	
-}
-
-#nav-list-example {
-    height: 132px;
-    width: 624px;
-    margin: 48px 0;
-}
-
-#nav-list-example li {
-    width: 132px;
-    height: 132px;
-    float: left;
-    margin-right: 24px;
-    position: relative;
-}
-
-#nav-list-example li div {
-    width: 132px;
-    height: 132px;
-    overflow: hidden;
-    position: absolute;
-    top: 0;
-    left: 0;
-}
-
-#nav-list-example li div.back {
-    left: -999em;
-    background: #999;
-}
-
-
-
-</style>
+<body>
 
 <div class="box1">Algebra</div>
 <div class="box2">Trigonometri</div>
 <div class="box3">Geometri</div>
-
-
-
-
-
-
 
 <script type="text/javascript">
 	 $(document).ready(function () {
