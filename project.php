a498390abf2f1b6a370c56d734da47523ea2a5b5
Fuwzie <zypeh@live.com>
2016-10-25 13:50:35 +0200

    nvm we arent done

--- project.php
+++ project.php
@@ -2,12 +2,7 @@
 <html>
 <head>
 	<title>Matteprojekt</title>
-
-<link rel="stylesheet" type="text/css" href="style.css">
-<script type="text/javascript" src="/js/jquery.js"></script>
-<script type="text/javascript" src="/js/base.js"></script>
-<script type="text/javascript" src="/js/rotate3di.js"></script>
-
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
